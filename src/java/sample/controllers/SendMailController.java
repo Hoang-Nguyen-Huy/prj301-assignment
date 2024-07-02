8f476fb3c7a1bbfee0ba2d0ad3e64491d5992067
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-07-02 16:56:19 +0700

    feat: send mail

---
+++ src/java/sample/controllers/SendMailController.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sample.mail.Email;
+
+/**
+ *
+ * @author Dell Latitude 7490
+ */
+@WebServlet(name = "SendMailController", urlPatterns = {"/SendMailController"})
+public class SendMailController extends HttpServlet {
+
+    private static final String ERROR = "CheckoutController";
+    private static final String SUCCESS = "CheckoutController";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            Email handleEmail = new Email();
+            String cusEmail = request.getParameter("emailToSent");
+            String msg = request.getParameter("orderToSent");
+            boolean sendMail = handleEmail.sendEmail("Đây là thông tin và mã đặt hàng của bạn", msg, cusEmail);
+            if (sendMail) {
+                url = SUCCESS;
+                request.setAttribute("SEND_MAIL_SUCCESS", "Please check your email");
+            } else {
+                request.setAttribute("SEND_MAIL_ERROR", "Something went wrong");
+            }
+        } catch (Exception e) {
+            log("Error at SendMailController: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).include(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
